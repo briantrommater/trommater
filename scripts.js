6f301f6574d8fa9031f49f1c95db6c533aeb0b22
cybershark <41755610+briantrommater@users.noreply.github.com>
2025-02-10 22:32:54 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 async function analyzeText() {
     const text = document.getElementById("inputText").value;
 
-    const response = await fetch("http://127.0.0.1:5000/analyze", {
+    const response = await fetch("https://briantrommater.com/analyze", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
