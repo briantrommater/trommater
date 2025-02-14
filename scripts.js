79a63e38f385658eb17dd412cd542c52e8a5bce5
brian_trommater <41755610+briantrommater@users.noreply.github.com>
2025-02-13 17:43:27 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -40,4 +40,38 @@ async function analyzeText() {
     } else {
         resultElement.classList.add("high-risk");  // Red
     }
-}
+    
+    }
+    const originalImage = [
+    "img1.jpg", "img2.jpg", "img3.jpg",
+    "img4.jpg", "img5.jpg", "img6.jpg",
+    "img7.jpg", "img8.jpg", "img9.jpg"
+        ];
+
+    let shuffledImage = [...originalImage];
+
+    function shuffleTiles() {
+    shuffledImage.sort(() => Math.random() - 0.5);
+    renderTiles();
+    }
+
+    function renderTiles() {
+    const grid = document.getElementById("captchaGrid");
+    grid.innerHTML = "";
+    shuffledImage.forEach((src, index) => {
+        let tile = document.createElement("div");
+        tile.classList.add("captcha-tile");
+        tile.style.backgroundImage = `url(${src})`;
+        grid.appendChild(tile);
+        });
+    }
+
+    function verifyCaptcha() {
+    if (JSON.stringify(shuffledImage) === JSON.stringify(originalImage)) {
+        document.getElementById("captchaResult").innerText = "✅ Correct!";
+    } else {
+        document.getElementById("captchaResult").innerText = "❌ Not yet correct!";
+    }
+    }
+
+    window.onload = renderTiles;
